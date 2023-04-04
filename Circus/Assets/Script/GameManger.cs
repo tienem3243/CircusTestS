7653f798af05f926de87916a9ae84af7299fe83b
Tien Emil <63142587+tienem3243@users.noreply.github.com>
2023-04-04 20:34:22 +0700

    add new singleton implement

--- Circus/Assets/Script/GameManger.cs
+++ Circus/Assets/Script/GameManger.cs
@@ -1,24 +1,16 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class GameManger : MonoBehaviour
+public class GameManger : MonoBehaviourSingleton<GameManger>
 {
-    public static GameManger Instance;
+  
     public bool isWin = false;
     public bool isLose = false;
     public bool canCut;
     public int countStart = 0;
     public UnityEvent OnWin;
     public UnityEvent OnLose;
-    protected virtual void Awake()
-    {
-        if (!Instance)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else Destroy(gameObject);
-    }
+
     enum GameState { Win, Lose };
 
     private void Start()
